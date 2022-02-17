b1d9cbc1b035c852447931bea73b677b0b6dd980
Reshmika Nambiar <reshmikasnambiar@gmail.com>
2022-02-17 14:52:58 +0530

    Added the other pages, edited manifest.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import {
   Route
 } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import SponsorMain from "./pages/SponsorMain/SponsorMain";
+import Competitions from "./pages/Competitions/Competitions"
+import Team from "./pages/Team/Team"
+import Merch from "./pages/Merch/Merch"
+import Gallery from './pages/Gallery/Gallery';
+import ContactUs from './pages/ContactUs/ContactUs';
 
 function App() {
   return (
@@ -13,6 +19,18 @@ function App() {
 				<Routes>
         <Route path={process.env.PUBLIC_URL} element={<Home />}>
         <Route index element={<Home />} />
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/sponsor`} element={<SponsorMain />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/competitions`} element={<Competitions />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/team`} element={<Team />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/merch`} element={<Merch />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/gallery`} element={<Gallery />}>
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/contact`} element={<ContactUs />}>
         </Route>
 				</Routes>
 			</div>
